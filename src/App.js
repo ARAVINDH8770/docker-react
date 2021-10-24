d412cff0106d641cd8b69ae12d6bd8c4320d42f3
ARAVINDH8770 <aravindhmp18it@drngpit.ac.in>
2021-10-25 04:59:15 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello Aravindh <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +20,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+}export default App;
